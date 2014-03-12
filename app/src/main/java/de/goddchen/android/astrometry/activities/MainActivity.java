b942b85a50913cf755158cf60d5a6891b1202e6d
Goddchen <goddchen@googlemail.com>
2014-03-12 19:46:23 +0100

    add Flurry

--- app/src/main/java/de/goddchen/android/astrometry/activities/MainActivity.java
+++ app/src/main/java/de/goddchen/android/astrometry/activities/MainActivity.java
@@ -3,8 +3,12 @@ package de.goddchen.android.astrometry.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
 
 import de.goddchen.android.astrometry.Application;
+import de.goddchen.android.astrometry.R;
 import de.goddchen.android.astrometry.fragments.JobListFragment;
 import de.goddchen.android.astrometry.fragments.LoginFragment;
 
@@ -27,4 +31,23 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            FlurryAgent.onStartSession(this, getString(R.string.id_flurry));
+        } catch (Exception e) {
+            Log.w(Application.Constants.LOG_TAG, "Error starting Flurry", e);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            FlurryAgent.onEndSession(this);
+        } catch (Exception e) {
+            Log.w(Application.Constants.LOG_TAG, "Error stopping Flurry", e);
+        }
+    }
 }
